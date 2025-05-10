bec0a44d15126d08b9df056cbef1e35458f76174
thrinisty <714605471@qq.com>
2025-05-10 20:49:29 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,8 +20,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://thrinisty.github.io/",
+	base: "Blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
